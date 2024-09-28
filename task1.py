288e9e04277882d7b98f63068dca3028124a8deb
alyonastr <146359188+alyonastr@users.noreply.github.com>
2024-09-28 07:32:15 +0000

    fjutik

--- task1.py
+++ task1.py
@@ -3,16 +3,24 @@ class SchoolJournal:
     def __init__(self, subject, student):
         self.subject = subject
         self.student = student
-        grade_list = []
+        self.grade_list = []
 
-    def grade(self):
-        g = int(input())
-        grade_list[-1] = g
+    def grade(self, g):
+        self.g = g 
+        if g >=1 and g<=5:
+            self.grade_list.append(g)
+
 
     def printer(self):
-        print(SchoolJournal.student)
-        print(SchoolJournal.subject)
-        print(grade_list)
+        print(self.student)
+        print(self.subject)
+        print(self.grade_list)
 
     def final_grade(self):
-        print(sum(grade_list)// len(grade_list))
+        print(sum(self.grade_list)/ len(self.grade_list))
+    
+funct = SchoolJournal('математика', 'юля')
+funct.grade(4)
+funct.grade(5)
+funct.printer()
+funct.final_grade()
---
+++ task2.py
@@ -0,0 +1,28 @@
+class Pyramid:
+    def __init__(self, max_h, bricks_count):
+        self.max_h = max_h
+        self.bricks_count = bricks_count
+    
+    def add_bricks(self,):
+        self.bricks += 1
+
+    def get_height(self):
+        /
+
+    def is_done(self):
+        /
+
+class Builder:
+
+    def __init__(self, bricks, my_pyramid):
+        self.bricks = bricks
+        self.my_pyramid = Pyramyd(15)
+    
+    def buy_bricks(self):
+        
+        
+    def build_pyramyd(self):
+
+    def work_day(self):
+
+        
