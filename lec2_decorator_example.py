c3ac8e8c63e9c2653cdb9363cf04bbcc5e9a4695
alyonastr <146359188+alyonastr@users.noreply.github.com>
2024-10-25 12:22:29 +0000

    jbm

---
+++ lec2_decorator_example.py
@@ -0,0 +1,13 @@
+def logger(func):
+    def wrapper_function(list_of_num):
+        result = func(list_of_num)
+        f = open('demofile.txt','w')
+        f.write(str(result))
+        f.close()
+        return result
+    
+@logger
+def summator(list_of_num):
+    return sum(list_of_num)
+
+summator([1, 2, 3, 4])
